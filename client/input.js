0b8fa1bd73ceb21f76004c5c9213e3de5a5980ee
Daniel Li <dan@danyll.com>
2015-05-17 01:52:04 +0800

    Fixed cross-browser key-binding issue

--- client/input.js
+++ client/input.js
@@ -1,7 +1,8 @@
 Template.footer.events({
-  'keypress input': function(event) {
-        if (event.charCode == 13) {
-            event.stopPropagation();
+  'keypress input': function(e) {
+  		var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+        if (charCode == 13) {
+            e.stopPropagation();
             $('.message-history').append('<div class="message"><a href="" class="message_profile-pic"></a><a href="" class="message_username">scotch</a><span class="message_timestamp">1:31 AM</span><span class="message_star"></span><span class="message_content">' + $('.input-box_text').val() + '</span></div>');
             $('.input-box_text').val("");
             return false;
