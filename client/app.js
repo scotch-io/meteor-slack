d8d4ee1f3d9f8ecd23a23e82291fd685243f3034
Daniel Li <dan@danyll.com>
2015-05-17 02:17:29 +0800

    Change client code to use collection instead

--- client/app.js
+++ client/app.js
@@ -1,11 +1,3 @@
 Template.messages.helpers({
-  messages: [
-    { text: "All these messages" },
-    { text: "Uses the same template" },
-    { text: "But have a different data context" },
-    { text: "It's why these messages are all different!" },
-    { text: "Hey!" },
-    { text: "What's up man!" },
-    { text: "Hello" }
-  ]
+  messages: Messages.find({})
 });
