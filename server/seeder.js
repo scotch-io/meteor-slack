f064017d3c56dde45b7517c71457982bc9bab8d2
Daniel Li <dan@danyll.com>
2015-05-17 02:15:34 +0800

    Added in Message collection and seeding it

---
+++ server/seeder.js
@@ -0,0 +1,17 @@
+Meteor.startup(function() {
+
+  Factory.define('message', Messages, {
+    text: function() {
+    	return Fake.sentence();
+    }
+  });
+
+  // Add this if you want to remove all messages before seeding
+  Messages.remove({});
+
+  if (Messages.find({}).count() === 0) {
+    _(10).times(function(n) {
+      Factory.create('message');
+    });
+  }
+});
