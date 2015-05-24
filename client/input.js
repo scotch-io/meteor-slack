a5beb380716dc354fa7e17a2f193f7f3e4efceb9
Daniel Li <dan@danyll.com>
2015-05-25 03:59:35 +0800

    Change from allow/deny rules to using methods

--- client/input.js
+++ client/input.js
@@ -5,11 +5,7 @@ Template.footer.events({
       var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
       if (charCode == 13) {
         e.stopPropagation();
-        Messages.insert({
-          text: $('.input-box_text').val(),
-          user: Meteor.userId(),
-          timestamp: Date.now()
-        });
+        Meteor.call('newMessage', {text: $('.input-box_text').val()});
         $('.input-box_text').val("");
         return false;
       }    
