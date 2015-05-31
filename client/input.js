dd702bdaefc92ded662aa9cdc5b5ff57face4568
Daniel Li <dan@danyll.com>
2015-06-01 00:36:40 +0800

    Update channel when clicked

--- client/input.js
+++ client/input.js
@@ -11,4 +11,11 @@ Template.footer.events({
       }    
     }
   }
+});});
+});
+
+Template.channel.events({
+  'click .channel': function (e) {
+    Session.set('channel', this.name);
+  }
 });
--- client/startup/subscribe.js
+++ client/startup/subscribe.js
@@ -1,1 +1,6 @@
+Meteor.startup(function() {
+  Session.set('channel', 'general');
+});
+
 Meteor.subscribe('channels');
+Meteor.subscribe('allUsernames');
