51ec9c40c336c9cfda08d8611000eb51cb192b51
Daniel Li <dan@danyll.com>
2016-01-23 14:31:45 +0800

    Scroll to bottom of messages after new message.

--- client/startup/subscribe.js
+++ client/startup/subscribe.js
@@ -4,6 +4,13 @@ Meteor.subscribe('allUsernames');
 Template.messages.onCreated(function() {
   var self = this;
   self.autorun(function() {
-    self.subscribe('messages', Session.get('channel'));
+    self.subscribe('messages', Session.get('channel'), function () {
+    	self.autorun(function() {
+			Messages.find({channel: Session.get('channel')}).count();
+			Meteor.setTimeout(function () {
+				$('.message-history').scrollTop(Number.MAX_VALUE);
+			}, 100);
+		});
+    });
   });
 });
